5d128329f36a66e0cdbed1a4bc1f31dfe546c5a0
shur <shur@192.168.5.3>
2023-11-13 22:20:47 +0800

    benchmark snippets

--- go/test/bench_test.go
+++ go/test/bench_test.go
@@ -1,1 +1,24 @@
 package main
+
+import "testing"
+
+func addOneNormal(i int) {
+	i = i + 1
+}
+func addOneAnd(i int) {
+	i += i & -i
+}
+
+func BenchmarkAddOneNormal(b *testing.B) {
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		addOneNormal(i)
+	}
+}
+
+func BenchmarkAddOneAnd(b *testing.B) {
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		addOneAnd(i)
+	}
+}
